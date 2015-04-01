18d5124a6309fe85d2eabdb16829e00d20b2a2eb
Kathy Lee Simunich <simunich@anl.gov>
2015-04-01 13:15:48 -0500

    Added pre-emptive basic authentication.
    Also added some things to let the project compile in Eclipse.

--- src/main/java/org/mitre/taxii/client/example/AbstractClient.java
+++ src/main/java/org/mitre/taxii/client/example/AbstractClient.java
@@ -5,14 +5,39 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+
 import javax.xml.bind.JAXBException;
+
 import org.apache.commons.cli.CommandLine;
+import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScheme;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
 import org.mitre.taxii.client.HttpClient;
 import org.mitre.taxii.messages.TaxiiXml;
 import org.mitre.taxii.messages.xml11.ObjectFactory;
@@ -32,10 +57,13 @@ abstract class AbstractClient {
     TaxiiXml taxiiXml;
     HttpClient taxiiClient;
     String defaultURL = "http://taxiitest.mitre.org/services/"; // If the "u" command line parameter is not provided, use this value.
-          
+
+    protected HttpClientContext context;
+    
+    
     AbstractClient() {
-        this.cli = new Cli();
-        taxiiXml = txf.createTaxiiXml();
+    	this.cli = new Cli();
+    	taxiiXml = txf.createTaxiiXml();
     }
     
     public Cli getCli() {
@@ -73,16 +101,30 @@ abstract class AbstractClient {
         if (cmd.hasOption("username") ^ cmd.hasOption("password")) {
             throw new Exception("'username' and 'password' arguments are required to appear together.");
         }
-        
+
+        URL targetURL = new URL(cmd.getOptionValue("u"));
+        HttpHost target = new HttpHost(targetURL.getHost(), 443, targetURL.getProtocol());
+
         if (cmd.hasOption("username") && cmd.hasOption("password")) {
+
             CredentialsProvider credsProvider = new BasicCredentialsProvider();
             credsProvider.setCredentials(
-                    AuthScope.ANY,
+            		new AuthScope(target.getHostName(), target.getPort()),
                     new UsernamePasswordCredentials(cmd.getOptionValue("username"), cmd.getOptionValue("password")));        
-            builder.setDefaultCredentialsProvider(credsProvider);                    
+            //builder.setDefaultCredentialsProvider(credsProvider); 
+            
+            // http://hc.apache.org/httpcomponents-client-ga/tutorial/html/authentication.html
+            AuthCache authCache = new BasicAuthCache();
+            authCache.put(target, new BasicScheme());
+             
+            // Add AuthCache to the execution context
+            context = HttpClientContext.create();
+            context.setCredentialsProvider(credsProvider);
+            context.setAuthCache(authCache);
         }
 
-/*   
+
+ /*   
 // TODO: Certificates and Key files is a rather rich and complicated subject. 
 // The python library supports a very specific implementation. Eventually support whatever the Python library supports.      
         
@@ -94,8 +136,29 @@ abstract class AbstractClient {
         if (cmd.hasOption("cert") && cmd.hasOption("key")) {
         }
 */        
+        
+        // from:  http://stackoverflow.com/questions/19517538/ignoring-ssl-certificate-in-apache-httpclient-4-3
+        SSLContextBuilder ssbldr = new SSLContextBuilder();
+        ssbldr.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(ssbldr.build(),SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+
+
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", new PlainConnectionSocketFactory())
+                .register("https", sslsf)
+                .build();
+
+
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
+        cm.setMaxTotal(20);//max connection
+
+        System.setProperty("jsse.enableSNIExtension", "false"); //""
+        CloseableHttpClient httpClient = builder
+                .setSSLSocketFactory(sslsf)
+                .setConnectionManager(cm)
+                .build();
                         
-        client.setHttpclient(builder.build());
+        client.setHttpclient(httpClient);
         return client;
     }    
     
@@ -109,7 +172,7 @@ abstract class AbstractClient {
             }
         
         // Call the service
-        Object responseObj = taxiiClient.callTaxiiService(new URI(cmd.getOptionValue("u", defaultURL)), request);
+        Object responseObj = taxiiClient.callTaxiiService(new URI(cmd.getOptionValue("u", defaultURL)), request, context);
 
         System.out.println("Response:");        
         if (cmd.hasOption("xmloutput")) {
--- src/main/java/org/mitre/taxii/client/example/PollClient.java
+++ src/main/java/org/mitre/taxii/client/example/PollClient.java
@@ -1,9 +1,12 @@
 package org.mitre.taxii.client.example;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -11,10 +14,15 @@ import java.io.Writer;
 import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -23,6 +31,7 @@ import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.mitre.taxii.ContentBindings;
@@ -37,6 +46,8 @@ import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSSerializer;
 
 public class PollClient extends AbstractClient {
+	private static final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 
     /**
      * @param args the command line arguments
@@ -75,6 +86,10 @@ public class PollClient extends AbstractClient {
         String endStr = cmd.getOptionValue("end_timestamp", null);
         String subId = cmd.getOptionValue("subscription_id", null);
         String dest = cmd.getOptionValue("dest_dir", ".");
+        
+		Date lastTime = getLatestTime(dest);
+		Date now = new Date();
+
 
         taxiiClient = generateClient(cmd);
 
@@ -88,7 +103,7 @@ public class PollClient extends AbstractClient {
         } else {
             request.withPollParameters(factory.createPollParametersType());
         }
-
+/**
         if (null != beginStr) {
             XMLGregorianCalendar beginTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(beginStr);
             request.setExclusiveBeginTimestamp(beginTime);
@@ -98,11 +113,39 @@ public class PollClient extends AbstractClient {
             XMLGregorianCalendar endTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(endStr);
             request.setInclusiveEndTimestamp(endTime);
         }
+ */
+        // use the gregorian calendar instead of the string format
+    	Calendar gc = GregorianCalendar.getInstance();
+    	gc.setTime(lastTime);
+    	XMLGregorianCalendar beginTime = DatatypeFactory.newInstance().newXMLGregorianCalendar((GregorianCalendar)gc).normalize();
+    	beginTime.setFractionalSecond(null);
+    	Logger.getLogger(PollClient.class.getName()).log(Level.INFO,"Begin Time: "+beginTime);
+        request.setExclusiveBeginTimestamp(beginTime);
+
+    	
+    	gc.setTime(now);
+    	XMLGregorianCalendar endTime = DatatypeFactory.newInstance().newXMLGregorianCalendar((GregorianCalendar)gc).normalize();
+    	endTime.setFractionalSecond(null);
+    	Logger.getLogger(PollClient.class.getName()).log(Level.INFO,"End Time: "+endTime);
+        request.setInclusiveEndTimestamp(endTime);
+
+
 
         Object response = doCall(cmd, request);
 
         if (response instanceof PollResponse) {
+        	// do I want to still do this for our use?  KLS
             handleResponse(dest, (PollResponse) response);
+            
+			// write out the returned results as a string. - this is for the FX translator
+//			File output = new File(dest + "/" + now.getTime() + "_" + "PollResults.xml");
+//			FileWriter fw = new FileWriter(output);
+//			fw.write(response.toString());
+//			fw.close();
+//			
+//			// write filepath to stdout, not the logger
+//			System.out.println(output.getCanonicalPath());
+
         }
     }
 
@@ -119,116 +162,178 @@ public class PollClient extends AbstractClient {
             String ext;
             Writer fileWriter = null;
             
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(true);
-            DocumentBuilder db = dbf.newDocumentBuilder();
+            List<ContentBlock> blocks =  response.getContentBlocks();
+            if (blocks.size() > 0) {
             
-            for (ContentBlock cb : response.getContentBlocks()) {
-                // Build the filename for the output.
-                try {
-                    String binding = cb.getContentBinding().getBindingId();
-                    if (ContentBindings.CB_STIX_XML_10.equals(binding)) {
-                        format = "_STIX10_";
-                        ext = ".xml";
-                    } else if (ContentBindings.CB_STIX_XML_101.equals(binding)) {
-                        format = "_STIX101_";
-                        ext = ".xml";
-                    } else if (ContentBindings.CB_STIX_XML_11.equals(binding)) {
-                        format = "_STIX11_";
-                        ext = ".xml";
-                    } else if (ContentBindings.CB_STIX_XML_111.equals(binding)) {
-                        format = "_STIX111_";
-                        ext = ".xml";
-                    } else { // Format and extension are unknown
-                        format = "";
-                        ext = "";
-                    }
-                    if (null != cb.getTimestampLabel()) {
-                        dateString = 't' + cb.getTimestampLabel().toXMLFormat(); // This probably won't work due to illegal characters.
-                    } else {
-                        try {
-                            GregorianCalendar gc = new GregorianCalendar();
-                            gc.setTime(new Date()); // Now.
-                            XMLGregorianCalendar now = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
-                            dateString = "s" + now.toXMLFormat();
-                        } catch (DatatypeConfigurationException ex) {
-                            dateString = "";
-                        }
-                    }
-                    
-                    // Construct the complete path to write the ContentBlock's content to.
-                    String filename = response.getCollectionName() + format + dateString + ext;
-                    // Remove characters that might make the OS unhappy with the filename.
-                    filename = filename.replaceAll("[\\*:<>\\/\\?|]", "");
-                    String filepath = dest + File.separator + filename;
-                    File outFile = new File(filepath);
-                    fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), "utf-8"));
-                    
-                    /* Marshal the ContentBlock to a DOM document.
-                    It must be done at this level because the child nodes are not
-                    defined as root elements in the schema. So we need to marshal
-                    the closest root element and then dig down to get to what we
-                    really want to marshal.
-                    */
-                    Marshaller m = taxiiXml.createMarshaller(true);
-                    Document doc = db.newDocument();
-                    m.marshal(cb, doc);
-                    
-                    /* NOTE: It is bad practice to rely on the namespace prefix being a certain value.
-                    But in this case, the JAXB binding configuration dictates what it will be.
-                    */
-                    NodeList contents = doc.getElementsByTagName("taxii_11:Content");
-                    
-                    /* According to the schema there must be exactly 1 Content element, but make sure we got one. */
-                    if (0 < contents.getLength()) {
-                        Node contentNode = contents.item(0);
-                        /*
-                        Content contains AnyMixedContentType. It is not necessarily a single element.
-                        And may be text & XML elements mixed together.
-                        */
-                        NodeList contentChildren = contentNode.getChildNodes();
-                        int numChildren = contentChildren.getLength();
-                        
-                        // System.out.println(numChildren + " Content children.");
-                        for (int count = 0; count < numChildren; count++) {
-                            
-                            Node child = contentChildren.item(count);
-                            // System.out.println("Processing child '" + child.getNodeName() + "': " + child.getNodeValue());
-                            
-                            DOMImplementationLS domImpl = (DOMImplementationLS)child.getOwnerDocument().getImplementation();
-                            LSSerializer serializer = domImpl.createLSSerializer();
-                            serializer.getDomConfig().setParameter("xml-declaration", false);
-                                                        
-                            // Write current child to a string.
-                            String childStr = serializer.writeToString(child);
-                            
-                            // Append child string to output file.
-                            fileWriter.append(childStr);
-                        }
-                        fileWriter.flush();
-                        fileWriter.close();
-                        System.out.println(String.format("Wrote Content to %s", filepath));;
-                    } // If Content element found.
-                } catch (UnsupportedEncodingException ex) {
-                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (JAXBException ex) {
-                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
-                } finally {
-                    if (null != fileWriter) {
-                        try {
-                            fileWriter.close();
-                        } catch (IOException ex) {
-                            Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                }
-            }// for each ContentBlock
+	            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	            dbf.setNamespaceAware(true);
+	            DocumentBuilder db = dbf.newDocumentBuilder();
+	            
+	            for (ContentBlock cb : blocks) {
+	                // Build the filename for the output.
+	                try {
+	                    String binding = cb.getContentBinding().getBindingId();
+	                    if (ContentBindings.CB_STIX_XML_10.equals(binding)) {
+	                        format = "_STIX10_";
+	                        ext = ".xml";
+	                    } else if (ContentBindings.CB_STIX_XML_101.equals(binding)) {
+	                        format = "_STIX101_";
+	                        ext = ".xml";
+	                    } else if (ContentBindings.CB_STIX_XML_11.equals(binding)) {
+	                        format = "_STIX11_";
+	                        ext = ".xml";
+	                    } else if (ContentBindings.CB_STIX_XML_111.equals(binding)) {
+	                        format = "_STIX111_";
+	                        ext = ".xml";
+	                    } else { // Format and extension are unknown
+	                        format = "";
+	                        ext = "";
+	                    }
+	                    if (null != cb.getTimestampLabel()) {
+	                        dateString = 't' + cb.getTimestampLabel().toXMLFormat(); // This probably won't work due to illegal characters.
+	                    } else {
+	                        try {
+	                            GregorianCalendar gc = new GregorianCalendar();
+	                            gc.setTime(new Date()); // Now.
+	                            XMLGregorianCalendar now = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+	                            dateString = "s" + now.toXMLFormat();
+	                        } catch (DatatypeConfigurationException ex) {
+	                            dateString = "";
+	                        }
+	                    }
+	                    
+	                    // Construct the complete path to write the ContentBlock's content to.
+	                    String filename = response.getCollectionName() + format + dateString + ext;
+	                    // Remove characters that might make the OS unhappy with the filename.
+	                    filename = filename.replaceAll("[\\*:<>\\/\\?|]", "");
+	                    String filepath = dest + File.separator + filename;
+	                    File outFile = new File(filepath);
+	                    fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), "utf-8"));
+	                    
+	                    /* Marshal the ContentBlock to a DOM document.
+	                    It must be done at this level because the child nodes are not
+	                    defined as root elements in the schema. So we need to marshal
+	                    the closest root element and then dig down to get to what we
+	                    really want to marshal.
+	                    */
+	                    Marshaller m = taxiiXml.createMarshaller(true);
+	                    Document doc = db.newDocument();
+	                    m.marshal(cb, doc);
+	                    m.marshal(cb,System.out);
+	                    
+	                    /* NOTE: It is bad practice to rely on the namespace prefix being a certain value.
+	                    But in this case, the JAXB binding configuration dictates what it will be.
+	                    */
+	                    NodeList contents = doc.getElementsByTagName("Content");
+	                    
+	                    /* According to the schema there must be exactly 1 Content element, but make sure we got one. */
+	                    if (0 < contents.getLength()) {
+	                        Node contentNode = contents.item(0);
+	                        /*
+	                        Content contains AnyMixedContentType. It is not necessarily a single element.
+	                        And may be text & XML elements mixed together.
+	                        */
+	                        NodeList contentChildren = contentNode.getChildNodes();
+	                        int numChildren = contentChildren.getLength();
+	                        
+	                        // System.out.println(numChildren + " Content children.");
+	                        for (int count = 0; count < numChildren; count++) {
+	                            
+	                            Node child = contentChildren.item(count);
+	                            // System.out.println("Processing child '" + child.getNodeName() + "': " + child.getNodeValue());
+	                            
+	                            DOMImplementationLS domImpl = (DOMImplementationLS)child.getOwnerDocument().getImplementation();
+	                            LSSerializer serializer = domImpl.createLSSerializer();
+	                            serializer.getDomConfig().setParameter("xml-declaration", false);
+	                                                        
+	                            // Write current child to a string.
+	                            String childStr = serializer.writeToString(child);
+	                            
+	                            // Append child string to output file.
+	                            fileWriter.append(childStr);
+	                        }
+	                        fileWriter.flush();
+	                        fileWriter.close();
+	                        System.out.println(String.format("Wrote Content to %s", filepath));;
+	                    } // If Content element found.
+	                } catch (UnsupportedEncodingException ex) {
+	                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
+	                } catch (JAXBException ex) {
+	                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
+	                } catch (FileNotFoundException ex) {
+	                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
+	                } catch (IOException ex) {
+	                    Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
+	                } finally {
+	                    if (null != fileWriter) {
+	                        try {
+	                            fileWriter.close();
+	                        } catch (IOException ex) {
+	                            Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
+	                        }
+	                    }
+	                }
+	            }// for each ContentBlock
+            } else {
+	            Logger.getLogger(PollClient.class.getName()).log(Level.WARNING,"There were no Content Blocks returned");
+	        }
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }// handleResponse()
+    
+	/**
+	 * Read file with last download timestamp and return Date.  Rewrite file with current date for next time.
+	 * @return Date
+	 */
+	public Date getLatestTime(String destDir) {
+		File dlDir = new File(destDir);
+		if (!dlDir.exists()) {
+			dlDir.mkdirs();
+		}
+
+		// I'd much prefer to use Joda-time or switch to Java 8's new Date/times.
+		//  there will be issues around daylight savings time since it's saving in local time
+		//  and not UTC.
+		Date lastTime = null;
+		BufferedReader rd = null;
+		lastTime = new Date();
+		try {
+			File fn = new File(dlDir,"lasttime.txt");
+			if (fn.exists()) {
+				// read the date from the last time this program was called.
+				rd = new BufferedReader(new FileReader(fn));
+				lastTime = fmt.parse(rd.readLine());
+			}
+			
+		} catch (ParseException e) {
+			Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE,"couldn't parse date, using current time", e);
+		} catch (FileNotFoundException e) {
+			Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE,"File doesn't exist, using current time", e);
+		} catch (IOException e) {
+			Logger.getLogger(PollClient.class.getName()).log(Level.SEVERE,"couldn't read lasttime file, using current time", e);
+		} finally {
+			if (rd != null)
+				try {
+					rd.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		
+		// write out the current date to the file
+		File fn = new File(dlDir,"lasttime.txt");
+		FileWriter fw;
+		try {
+			fw = new FileWriter(fn);
+			fw.write(fmt.format(new Date()));
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return lastTime;
+	}
 }
