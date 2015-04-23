79c928b36055b0b7c4046ed8b47d541b7ced7577
Kathy Lee Simunich <simunich@anl.gov>
2015-04-23 14:51:36 -0500

    added logging to script

--- scripts/ANLinbox.sh
+++ scripts/ANLinbox.sh
@@ -12,9 +12,29 @@
 #
 # if having trouble with ssl, try running with this property:  -Djavax.net.debug=ssl,handshake
 #
-java -Dlog4j.configurationFile=../log4j2.xml -cp ../java-taxii-clients-all.jar \
- org.mitre.taxii.client.example.InboxClient -content_binding $1 \
--username $2 -password $3 -env $4 \
+
+IFILE=$(basename $5)
+DFILE=$(cut -d - -f 4- <<< "$IFILE")
+OFILE=$(cut -d . -f 1 <<< "$DFILE")
+
+ARGS='env="PROD" level="INFO" process="ANLinbox" state="START"'
+logger "$ARGS sessionID=\"$OFILE\"" -t cfm-anlinbox -p local1.info
+
+TEXT=$(java \
+-Dlog4j.configurationFile=/home/cfm/SoltraClient/log4j2.xml \
+-cp /home/cfm/SoltraClient/java-taxii-clients-all.jar \
+org.mitre.taxii.client.example.InboxClient -content_binding $1 \
+-username $2 \
+-password $3 \
+-env $4 \
 -content_file $5 \
 -destination_collection $6 \
--u $7 -xmloutput
+-u $7 \
+-xmloutput 2>&1)
+ARGS='env="PROD" level="INFO" process="ANLinbox" state="PROCESSING"'
+logger "$ARGS text=\"$TEXT\"" -t cfm-anlinbox -p local1.info
+
+ARGS='env="PROD" level="INFO" process="ANLinbox" state="END"'
+logger "$ARGS sessionID=\"$OFILE\"" -t cfm-anlinbox -p local1.info
+
+exit $?
